7d51ef2fe246e5166bdd10c8cb192bf4037932cf
Alberto <albertomutcirilo@gmail.com>
2021-12-01 19:21:25 +0100

    Day one - part two finished

--- src/main/java/com/alberto/advent/dayone/Counter.java
+++ src/main/java/com/alberto/advent/dayone/Counter.java
@@ -1,19 +1,16 @@
 package com.alberto.advent.dayone;
 
-import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
 public class Counter{
 
-    public Long measure() throws IOException{
-        List<Long> measurements = this.parseFile();
+    public Long measure(List<Long> measurements){
         long larger = 0L;
 
         for(int i = 1; i < measurements.size(); i++) {
@@ -25,11 +22,21 @@ public class Counter{
                 larger++;
             }
         }
-
         return larger;
     }
 
-    private List<Long> parseFile() throws IOException{
+    public List<Long> createWindows(List<Long> measurements){
+        List<Long> windows = new ArrayList<>();
+
+        for(int i = 0; i < measurements.size(); i++) {
+            if(i < (measurements.size() - 2)) {
+                windows.add(measurements.get(i) + measurements.get(i+1) + measurements.get(i+2));
+            }
+        }
+        return windows;
+    }
+
+    public List<Long> createMeasurements() throws IOException{
         Path fileName = Path.of("src/main/resources/001.txt");
         String content = Files.readString(fileName);
         String[] stringArray = content.split("\\r?\\n");
--- src/test/java/com/alberto/advent/dayone/CounterTest.java
+++ src/test/java/com/alberto/advent/dayone/CounterTest.java
@@ -4,12 +4,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 public class CounterTest{
 
     @Test
     public void test_00() throws IOException{
         Counter counter = new Counter();
-        Assertions.assertEquals(1722, counter.measure());
+        List<Long> measurements = counter.createMeasurements();
+        Assertions.assertEquals(1722, counter.measure(measurements));
+    }
+
+    @Test
+    public void test_01() throws IOException{
+        Counter counter = new Counter();
+        List<Long> measurements = counter.createMeasurements();
+        List<Long> windows = counter.createWindows(measurements);
+        Assertions.assertEquals(1748, counter.measure(windows));
     }
 }
